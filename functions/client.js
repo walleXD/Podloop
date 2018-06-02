133839268afa2a43a79c0ba8ef041e94e02255df
Walee Ahmed <dev@walee.me>
2018-06-02 09:34:08 -0400

    added async/await support

--- functions/client.js
+++ functions/client.js
@@ -5,12 +5,14 @@ const dev = process.env.NODE_ENV !== "production"
 const app = next({ dev, conf: { distDir: "next", dir: "./client.js" } })
 const handle = app.getRequestHandler()
 
-const nextApp = https.onRequest((request, response) => {
+const nextApp = https.onRequest(async (request, response) => {
   console.log("File: " + request.originalUrl)
   console.log("dev:", dev)
   // log the page.js file or resource being requested
 
-  return app.prepare().then(() => handle(request, response))
+  await app.prepare()
+
+  return handle(request, response)
 })
 
 export { nextApp }
--- index.js
+++ index.js
@@ -1,2 +1,3 @@
+require("async-to-gen/register")
 require = require("esm")(module) // eslint-disable-line
 module.exports = require("./functions")
